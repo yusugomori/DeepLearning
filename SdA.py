c4765a9a3c02f941216d49ccc31523478fb268ef
Yusuke Sugomori <restinthenest@gmail.com>
2013-02-06 18:08:51 +0900

    SdA

---
+++ SdA.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+ Stacked Denoising Autoencoders (SdA)
+
+ References :
+   - P. Vincent, H. Larochelle, Y. Bengio, P.A. Manzagol: Extracting and
+   Composing Robust Features with Denoising Autoencoders, ICML' 08, 1096-1103,
+   2008
+
+   - DeepLearningTutorials
+   https://github.com/lisa-lab/DeepLearningTutorials
+   
+"""
+
+import sys
+import numpy
+from HiddenLayer import HiddenLayer
+from LogisticRegression import LogisticRegression
+from dA import dA
+from utils import *
+
+
+class SdA(object):
+    def __init__(self, input=None, label=None,\
+                 n_ins=2, hidden_layer_sizes=[3, 3], n_outs=2,\
+                 numpy_rng=None):
+        
+        self.x = input
+        self.y = label
+
+        self.sigmoid_layers = []
+        self.dA_layers = []
+        self.n_layers = len(hidden_layer_sizes)  # = len(self.rbm_layers)
+
+        if numpy_rng is None:
+            numpy_rng = numpy.random.RandomState(1234)
+
+        
+        assert self.n_layers > 0
+
+
+        # construct multi-layer
+        for i in xrange(self.n_layers):
+            # layer_size
+            if i == 0:
+                input_size = n_ins
+            else:
+                input_size = hidden_layer_sizes[i - 1]
+
+            # layer_input
+            if i == 0:
+                layer_input = self.x
+            else:
+                layer_input = self.sigmoid_layers[-1].sample_h_given_v()
+                
+            # construct sigmoid_layer
+            sigmoid_layer = HiddenLayer(input=layer_input,
+                                        n_in=input_size,
+                                        n_out=hidden_layer_sizes[i],
+                                        numpy_rng=numpy_rng,
+                                        activation=sigmoid)
+            self.sigmoid_layers.append(sigmoid_layer)
+
+
+            # construct rbm_layer
+            # rbm_layer = RBM(input=layer_input,
+            #                 n_visible=input_size,
+            #                 n_hidden=hidden_layer_sizes[i],
+            #                 W=sigmoid_layer.W,     # W, b are shared
+            #                 hbias=sigmoid_layer.b)
+            # self.rbm_layers.append(rbm_layer)
+            
+            # construct dA_layers
+            dA_layer = dA(input=layer_input,
+                          n_visible=input_size,
+                          n_hidden=hidden_layer_sizes[i],
+                          W=sigmoid_layer.W,
+                          hbias=sigmoid_layer.b)
+            self.dA_layers.append(dA_layer)
+
+
+        # layer for output using Logistic Regression
+        self.log_layer = LogisticRegression(input=self.sigmoid_layers[-1].sample_h_given_v(),
+                                            label=self.y,
+                                            n_in=hidden_layer_sizes[-1],
+                                            n_out=n_outs)
+
+        # finetune cost: the negative log likelihood of the logistic regression layer
+        self.finetune_cost = self.log_layer.negative_log_likelihood()
+
+
+    def pretrain(self, lr=0.1, corruption_level=0.3, epochs=100):
+        for i in xrange(self.n_layers):
+            if i == 0:
+                layer_input = self.x
+            else:
+                layer_input = self.sigmoid_layers[i-1].sample_h_given_v(layer_input)
+
+            da = self.dA_layers[i]
+
+            for epoch in xrange(epochs):
+                da.train(lr=lr, corruption_level=corruption_level, input=layer_input)
+
+    def finetune(self, lr=0.1, epochs=100):
+        layer_input = self.sigmoid_layers[-1].sample_h_given_v()
+
+        # train log_layer
+        epoch = 0
+
+        while epoch < epochs:
+            self.log_layer.train(lr=lr, input=layer_input)
+            # self.finetune_cost = self.log_layer.negative_log_likelihood()
+            # print >> sys.stderr, 'Training epoch %d, cost is ' % epoch, self.finetune_cost
+            
+            lr *= 0.95
+            epoch += 1
+
+
+    def predict(self, x):
+        layer_input = x
+        
+        for i in xrange(self.n_layers):
+            sigmoid_layer = self.sigmoid_layers[i]
+            # rbm_layer = self.rbm_layers[i]
+            layer_input = sigmoid_layer.output(input=layer_input)
+
+        out = self.log_layer.predict(layer_input)
+        return out
+
+
+
+def test_SdA(pretrain_lr=0.1, pretraining_epochs=1000, corruption_level=0.3, \
+             finetune_lr=0.1, finetune_epochs=200):
+    x = numpy.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0]])
+
+    y = numpy.array([[1, 0],
+                     [1, 0],
+                     [1, 0],
+                     [1, 0],
+                     [1, 0],
+                     [0, 1],
+                     [0, 1],
+                     [0, 1],
+                     [0, 1],
+                     [0, 1]])
+
+
+    rng = numpy.random.RandomState(123)
+
+    # construct SdA
+    sda = SdA(input=x, label=y, \
+              n_ins=20, hidden_layer_sizes=[10, 10], n_outs=2, numpy_rng=rng)
+
+    # pre-training
+    sda.pretrain(lr=pretrain_lr, corruption_level=corruption_level, epochs=pretraining_epochs)
+
+    # fine-tuning
+    sda.finetune(lr=finetune_lr, epochs=finetune_epochs)
+
+
+    # test
+    x = numpy.array([[1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1]])
+
+    print sda.predict(x)
+    
+    
+
+if __name__ == "__main__":
+    test_SdA()
